2ec11482b5adfb1d2578325501b074cd71672b18
DYankova <dilyana89yankova@gmail.com>
2020-02-25 15:43:09 +0200

    align code

--- BreakingBadCharactersTests/CharacterViewModelTests.swift
+++ BreakingBadCharactersTests/CharacterViewModelTests.swift
@@ -13,8 +13,6 @@ class CharacterViewModelTests: XCTestCase {
     
     var characterViewModel: CharacterViewModel!
     var testDictOne:  [String: Any] = ["char_id":1,"name":"Rachel","occupation":["designer"],"img":"https://pmctvline2.files.wordpress.com/2019/06/friends-rachel.jpg?w=620","status":"Deceased","nickname":"Rachel","appearance":[2,3,4],]
-    var testDictTwo:  [String: Any] =  ["char_id":2,"name":"Monica","occupation":["cook"],"img":"https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234","status":"Deceased","nickname":"Mon","appearance":[2,3],]
-    var testDictThree:  [String: Any] = ["char_id":3,"name":"Feeby","occupation":["designer"],"img":"https://hips.hearstapps.com/digitalspyuk.cdnds.net/14/36/ustv-friends-10.jpg?crop=1.00xw:0.735xh;0,0.0217xh&resize=480:*","status":"Deceased","nickname":"Feens","appearance":[2,3,5],]
     
     override func setUp() {
         characterViewModel = CharacterViewModel(CharacterModel(testDictOne))
--- BreakingBadCharactersTests/CharsViewModelTest.swift
+++ BreakingBadCharactersTests/CharsViewModelTest.swift
@@ -15,15 +15,15 @@ class CharsViewModelTest: XCTestCase {
     
     var testCharVMOne = CharacterViewModel(CharacterModel( ["char_id":1,"name":"Rachel","occupation":["designer"],"img":"https://pmctvline2.files.wordpress.com/2019/06/friends-rachel.jpg?w=620","status":"Deceased","nickname":"Rachel","appearance":[2,3,4]]))
     
-    var testCharVMTwo = CharacterViewModel(CharacterModel( ["char_id":2,"name":"Monica","occupation":["cook"],"img":"https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234","status":"Deceased","nickname":"Mon","appearance":[2,3]]))
+    var testCharVMTwo = CharacterViewModel(CharacterModel( ["char_id":2,"name":"Monica","occupation":["cook"],"img":"https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234","status":"Deceased","nickname":"Mon","appearance":[2,4]]))
     
     var testCharVMThree = CharacterViewModel(CharacterModel( ["char_id":3,"name":"Feeby","occupation":["designer"],"img":"https://hips.hearstapps.com/digitalspyuk.cdnds.net/14/36/ustv-friends-10.jpg?crop=1.00xw:0.735xh;0,0.0217xh&resize=480:*","status":"Deceased","nickname":"Feens","appearance":[3,5]]))
     
     override func setUp() {
         charsViewModel = AllCharsViewModel()
-//        charsViewModel.currentCharViewModels.append(testCharVMOne)
-//        charsViewModel.currentCharViewModels.append(testCharVMTwo)
-//        charsViewModel.currentCharViewModels.append(testCharVMThree)
+        charsViewModel.currentCharViewModels.append(testCharVMOne)
+        charsViewModel.currentCharViewModels.append(testCharVMTwo)
+        charsViewModel.currentCharViewModels.append(testCharVMThree)
     }
 
     override func tearDown() {
@@ -31,40 +31,32 @@ class CharsViewModelTest: XCTestCase {
     }
 
     func testfilterByNameContains(){
-        charsViewModel.currentCharViewModels.append(testCharVMTwo)
         charsViewModel.filterByName("Monica")
-        XCTAssertEqual(charsViewModel.currentCharViewModels, charsViewModel.currentCharViewModels)
+        XCTAssertEqual(charsViewModel.currentCharViewModels, [testCharVMTwo])
     }
     
     func testfilterByNameNotContains(){
-        charsViewModel.currentCharViewModels.append(testCharVMTwo)
         charsViewModel.filterByName("Di")
         XCTAssertTrue(charsViewModel.currentCharViewModels.count == 0)
     }
     
     func testfilterByNameEmpty(){
-        charsViewModel.currentCharViewModels.append(testCharVMTwo)
         charsViewModel.filterByName("")
         XCTAssertTrue(charsViewModel.currentCharViewModels.count == 0)
     }
     
     func testfilterByNameContainsMany(){
-        charsViewModel.currentCharViewModels.append(testCharVMOne)
-        charsViewModel.currentCharViewModels.append(testCharVMThree)
+     
         charsViewModel.filterByName("e")
-        XCTAssertEqual(charsViewModel.currentCharViewModels, charsViewModel.currentCharViewModels)
+        XCTAssertEqual(charsViewModel.currentCharViewModels, [testCharVMOne, testCharVMThree])
     }
     
     func testfilterBySeasonContains(){
-        charsViewModel.currentCharViewModels.append(testCharVMOne)
-        charsViewModel.currentCharViewModels.append(testCharVMThree)
         charsViewModel.filterBySeason(3)
-        XCTAssertEqual(charsViewModel.currentCharViewModels, charsViewModel.currentCharViewModels)
+        XCTAssertEqual(charsViewModel.currentCharViewModels, [testCharVMOne, testCharVMThree])
     }
     
     func testfilterBySeasonNotContains(){
-        charsViewModel.currentCharViewModels.append(testCharVMOne)
-        charsViewModel.currentCharViewModels.append(testCharVMThree)
         charsViewModel.filterBySeason(7)
         XCTAssertTrue(charsViewModel.currentCharViewModels.count == 0)
     }
